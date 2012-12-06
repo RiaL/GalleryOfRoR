ca01a7376fef8f33a087acde0a01c48eb359f3ff
RiaL <nowaksson@gmail.com>
2012-12-06 17:29:26 +0100

    stworzenie projektu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GalleryOfRoR::Application.config.secret_token = '2b3596a9b571e550c827c75b81c77c83fb6141d099fd8696712f0f71561ed7e5a024fd5602b56ca1d7d642f06a9ad6f3c1b8b7ae5451f55f955e0971fd0bea6a'
